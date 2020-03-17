2fd157972c9703a4251f475e9679c52a62b65512
楼云龙 <lianer@live.cn>
2020-03-17 15:11:49 +0800

    update

--- src/leetcode/122-2.ts
+++ src/leetcode/122-2.ts
@@ -62,3 +62,5 @@ const maxProfit = function(prices: number[]): number {
 console.log(maxProfit([7, 1, 5, 3, 6, 4]), 7);
 console.log(maxProfit([1, 2, 3, 4, 5]), 4);
 console.log(maxProfit([7, 6, 4, 3, 1]), 0);
+
+export { maxProfit };
